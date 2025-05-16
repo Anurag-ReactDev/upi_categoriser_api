c20d747d1fde4be0fc3532b731bad9afa237bae9
Anurag_Ponnada <ponnadaanurag@gmail.com>
2025-05-16 19:04:47 +0530

    post request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,3 +8,12 @@ def say_hello():
     return (
         {"message":"👋Hello from FastAPI"}
         )
+
+#define input schema
+class GreetInput(BaseModel):
+    name :str
+
+@app.post("/greet")
+def greetUser(input:GreetInput):
+    # input is now a Python object, not raw JSON
+    return {"message":f"Hello {input.name}"}
